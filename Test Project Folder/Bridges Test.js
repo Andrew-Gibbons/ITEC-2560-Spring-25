84c46d0d7697c43d65187f1ad5bec50c93e40452
Andrew Gibbons <andrew.gibbons.work@gmail.com>
2025-03-30 11:25:36 -0500

    lbridge icon separate js

---
+++ Test Project Folder/Bridges Test.js
@@ -0,0 +1,33 @@
+// Data for the bridges
+let bridges = [
+    {
+        name: "Verrazzano-Narrows Bridge",
+        city: "New York, NY",
+        span: 1298.4,
+        location: [40.6066, -74.0447]
+    },
+    {
+        name: "Golden Gate Bridge",
+        city: "San Francisco and Marin, CA",
+        span: 1280.2,
+        location: [37.8199, -122.4783]
+    },
+    {
+        name: "Mackinac Bridge",
+        city: "Mackinaw and St Ignace, MI",
+        span: 1158.0,
+        location: [45.8174, -84.7278]
+    },
+    {
+        name: "George Washington Bridge",
+        city: "New York, NY and New Jersey, NJ",
+        span: 1067.0,
+        location: [40.8517, -73.9527]
+    },
+    {
+        name: "Tacoma Narrows Bridge",
+        city: "Tacoma and Kitsap, WA",
+        span: 853.44,
+        location: [47.2690, -122.5517]
+    }
+];
