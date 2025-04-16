88c2cfb9dc981102e4774e007872ae3051f9cd52
Andrew Gibbons <andrew.gibbons.work@gmail.com>
2025-04-16 17:47:50 -0500

    Week 12 Lab

---
+++ Week 12 Web Servers API Servers w Express/routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function(req, res, next) {
+    res.render('index', {title: 'Feedback app', author: 'Andrew'})
+})
+
+router.get('/feedback-form', function(req, res, next){
+    res.render('student_feedback_form')
+})
+
+router.post('/submit-feedback', function(req, res, next){
+    // get the form data from the URL query 
+    const formData = req.body
+    
+    // TODO - save this data to a database so someone can read and respond 
+
+    res.render('thank_you', { 
+        name: formData.name, 
+        email: formData.email, 
+        message: formData.comments,
+        currentStudent: formData['current-student']
+     })
+
+})
+
+module.exports = router
