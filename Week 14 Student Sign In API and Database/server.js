38706ed52c3e20851215cfd2db24ce00ab300c24
Andrew Gibbons <andrew.gibbons.work@gmail.com>
2025-05-06 16:33:27 -0500

    Last commit before Azure deployment

--- Week 14 Student Sign In API and Database/server.js
+++ Week 14 Student Sign In API and Database/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const apiRoutes = require('./routes/api.js')
+const path = require('path')
 
 // Create web application server
 const app = express()
 
 app.use(express.json()) // Parse JSON request body
+
+const staticFilePath = path.join(__dirname, 'client', 'dist') 
+const staticFiles = express.static(staticFilePath)
+app.use('/', staticFiles)
 
 app.use('/api', apiRoutes) // Use API routes
 
